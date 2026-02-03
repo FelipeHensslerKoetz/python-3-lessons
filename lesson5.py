2e17fdb65e7139a489722fd5a371f9cb0347a12b
Felipe Henssler Koetz <felipehenssler@gmail.com>
2026-02-02 22:53:12 -0300

    feat: add additional boolean examples in lesson 5

--- lesson5.py
+++ lesson5.py
@@ -3,3 +3,5 @@ print(True)        # boolean True
 print(False)       # boolean False
 print(type(True))  # print type of boolean
 print(type(False)) # print type of boolean
+print(10 == 10) # equality comparison -> boolean result
+print(3 > 10) # greater than comparison -> boolean result
