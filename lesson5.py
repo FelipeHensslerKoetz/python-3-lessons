1273289161f57457def7aa8a28b6830d8e560111
Felipe Henssler Koetz <felipehenssler@gmail.com>
2026-02-02 22:51:27 -0300

    feat: add Boolean type examples in lesson 5

---
+++ lesson5.py
@@ -0,0 +1,5 @@
+# Boolean type examples
+print(True)        # boolean True
+print(False)       # boolean False
+print(type(True))  # print type of boolean
+print(type(False)) # print type of boolean
