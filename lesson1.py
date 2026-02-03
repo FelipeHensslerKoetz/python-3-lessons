ccebeea28aba07f138b2e0d1c25f56685cff708c
Felipe Henssler Koetz <felipehenssler@gmail.com>
2026-02-02 22:37:40 -0300

    chore: lesson 1 and 2

---
+++ lesson1.py
@@ -0,0 +1,6 @@
+""""
+  This is a DOC string.
+  This should not be use to comment code.
+"""
+
+# Single line comment - this line won't be executed
---
+++ lessson2.py
@@ -0,0 +1,3 @@
+print('Hello, World!') # print something on output
+print(1,2, sep="+", end="\n \n")
+print(3,4)
