96931f6851ffef6c6a794dd7a3ba860356d6e59f
Felipe Henssler Koetz <felipehenssler@gmail.com>
2026-02-02 22:45:55 -0300

    feat: add examples for lesson 2, lesson 3, and lesson 4

---
+++ lesson3.py
@@ -0,0 +1,8 @@
+# Type: String Examples
+print("Hello, World!") # string
+print('Hello, World!') # string
+print('Hello, "World!"') # string
+print("Hello, 'World!'") # string
+print("Hello, \"World!\"") # string
+print(r"Hello, \"World!\"") # string
+print(type("Hello, World!")) # print type of string
---
+++ lesson4.py
@@ -0,0 +1,5 @@
+# Numeric types examples
+print(10)          # integer
+print(type(10))  # print type of integer
+print(3.14)       # float
+print(type(3.14)) # print type of float
