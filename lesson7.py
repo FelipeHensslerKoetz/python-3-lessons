f8f5052882991d6ab465a66cbd333c0f76341dbc
Felipe Henssler Koetz <felipehenssler@gmail.com>
2026-02-05 18:38:08 -0300

    feat: add lesson 7 with variable examples and operations

---
+++ lesson7.py
@@ -0,0 +1,24 @@
+# Variables are used to store data that can be referenced and manipulated in a program.
+
+first_name = "John"  # A string variable to store a first name
+age = 30             # An integer variable to store age
+height = 5.9        # A float variable to store height in feet
+is_student = False   # A boolean variable to indicate if the person is a student
+
+# Printing the variables to the console
+print("First Name:", first_name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+# Performing some operations with the variables
+next_year_age = age + 1
+print("Age next year:", next_year_age)
+
+# Updating the height variable
+height_in_cm = height * 30.48  # Convert height to centimeters
+print("Height in cm:", height_in_cm)
+
+# Changing the is_student variable
+is_student = True
+print("Is Student (after update):", is_student)
